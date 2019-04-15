1782f82762b73af6d077d21c61363919f56c7212
grace <tianweigrace@qq.com>
2019-04-15 13:17:19 +0800

    add TestCase for Views

--- student/tests.py
+++ student/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from .models import Student
 
@@ -15,6 +15,7 @@ class StudentTestCse(TestCase):
             phone='9090950',
         )
 
+    """以下case测试models"""
     def test_create_and_sex_show(self):
         student = Student.objects.create(
             name='stu2',
@@ -38,3 +39,26 @@ class StudentTestCse(TestCase):
         name = 'stu2'
         students = Student.objects.filter(name=name)
         self.assertEqual(students.count(), 1, '应该只存在一个名称为{}的记录'.format(name))
+
+    """以下case测试views"""
+    def test_get_index(self):
+        client = Client()
+        res = client.get('/')
+        self.assertEqual(res.status_code, 200, 'status code must be 200')
+
+    def test_post_student(self):
+        client = Client()
+        data = dict(
+            name='test_for_post',
+            sex=1,
+            email='stu3@qq.com',
+            profession='teacher',
+            qq='123',
+            phone='12335'
+        )
+        # 既验证返回status code还验证内容结果
+        res = client.post('/', data)
+        self.assertEqual(res.status_code, 302, 'status code must be 302')
+
+        res = client.get('/')
+        self.assertTrue(b'test_for_post' in res.content, 'response content must contain "test_for_post"')
